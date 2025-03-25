420128ad750eeba9b931706df43df3d87c13484b
M-Busk <125284318+M-Busk@users.noreply.github.com>
2025-03-25 11:36:51 +0100

    start upgrading edc

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -18,21 +18,6 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 repositories {
-	
-    maven {// while runtime-metamodel dependency is still a snapshot
-		url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-    }
-	  maven {
-        url = uri("https://maven.iais.fraunhofer.de/artifactory/eis-ids-public/")
-    }
-	maven {
-		url = uri("https://maven.pkg.github.com/Digital-Ecosystems/edc-ionos-s3/")
-		credentials {
-			username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME_GITHUB")
-			password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN_GITHUB")
-		}
-	}
-	
 	mavenLocal()
 	mavenCentral()
 }
@@ -44,53 +29,58 @@ val ionosExtensionVersion: String by project
 
 
 dependencies {
-
-	implementation("${edcGroup}:boot:${edcVersion}")
 	implementation("${edcGroup}:control-plane-core:${edcVersion}")
-	implementation("${edcGroup}:control-plane-api:${edcVersion}")
-	implementation("${edcGroup}:control-plane-api-client:${edcVersion}")
-	implementation("${edcGroup}:api-observability:${edcVersion}")
 	implementation("${edcGroup}:configuration-filesystem:${edcVersion}")
-
 	implementation("${edcGroup}:auth-tokenbased:${edcVersion}")
 	implementation("${edcGroup}:management-api:${edcVersion}")
-
-	//implementation("${edcGroup}:iam-mock:${edcVersion}")
-
-	implementation("${edcGroup}:oauth2-service:${edcVersion}")
-	implementation("${edcGroup}:oauth2-daps:${edcVersion}")
-	implementation("${edcGroup}:vault-filesystem:${edcVersion}")
-
+	implementation("${edcGroup}:control-plane-api:${edcVersion}")
+	implementation("${edcGroup}:management-api-configuration:${edcVersion}")
+	implementation("${edcGroup}:api-observability:${edcVersion}")
 	implementation("${edcGroup}:dsp:${edcVersion}")
-	//file-transfer
-	implementation("${edcGroup}:data-plane-core:${edcVersion}")
-	implementation("${edcGroup}:data-plane-client:${edcVersion}")
-	implementation("${edcGroup}:data-plane-selector-client:${edcVersion}")
-	implementation("${edcGroup}:data-plane-selector-core:${edcVersion}")
+	implementation("${edcGroup}:jwt-spi:${edcVersion}")
 	implementation("${edcGroup}:transfer-data-plane:${edcVersion}")
-	implementation("${edcGroup}:data-plane-http:${edcVersion}")
+	implementation("${edcGroup}:data-plane-selector-core:${edcVersion}")
+	implementation("${edcGroup}:data-plane-selector-client:${edcVersion}")
+	implementation("${edcGroup}:data-plane-selector-spi:${edcVersion}")
 	implementation("${edcGroup}:http:${edcVersion}")
+	implementation("${edcGroup}:micrometer-core:${edcVersion}")
+	implementation("${edcGroup}:jersey-micrometer:${edcVersion}")
+	implementation("${edcGroup}:jetty-micrometer:${edcVersion}")
+	implementation("${edcGroup}:transfer-pull-http-dynamic-receiver:${edcVersion}")
+	implementation("${edcGroup}:callback-event-dispatcher:${edcVersion}")
+	implementation("${edcGroup}:callback-http-dispatcher:${edcVersion}")
 
+	implementation("${edcGroup}:data-plane-aws-s3:${edcVersion}")
+	implementation("${edcGroup}:data-plane-http-oauth2:${edcVersion}")
+	implementation("${edcGroup}:data-plane-http:${edcVersion}")
+	implementation("${edcGroup}:data-plane-core:${edcVersion}")
+	implementation("${edcGroup}:control-plane-api-client:${edcVersion}")
+	implementation("${edcGroup}:data-plane-api:${edcVersion}")
+	implementation("${edcGroup}:connector-core:${edcVersion}")
+	implementation("${edcGroup}:boot:${edcVersion}")
+
+
+	implementation("${edcGroup}:vault-hashicorp:${edcVersion}")
+	//implementation("${edcGroup}:vault-filesystem:${edcVersion}")
+	implementation("${edcGroup}:iam-mock:${edcVersion}")
+	//implementation("${edcGroup}:oauth2-service:${edcVersion}")
+	//implementation("${edcGroup}:oauth2-daps:${edcVersion}")
 	implementation(project(":policy-extension"))
 
-	//IONOS
-	//implementation ("com.ionoscloud.edc:provision-ionos-s3:${ionosExtensionVersion}")
-	//implementation ("com.ionoscloud.edc:data-plane-ionos-s3:${ionosExtensionVersion}")
-	implementation("org.eclipse.edc:aws-s3-core:0.4.1")
-	implementation("org.eclipse.edc:data-plane-aws-s3:0.4.1")
-	implementation("org.eclipse.edc:provision-aws-s3:0.4.1")
+	//implementation("software.amazon.awssdk:s3:2.20.75")
+	//implementation("${edcGroup}:provision-aws-s3:${edcVersion}")
 
-  implementation("${edcGroup}:asset-index-sql:${edcVersion}")
-  implementation("${edcGroup}:contract-definition-store-sql:${edcVersion}")
-  implementation("${edcGroup}:contract-negotiation-store-sql:${edcVersion}")
-  implementation("${edcGroup}:contract-core:${edcVersion}")
-  implementation("${edcGroup}:policy-definition-store-sql:${edcVersion}")
-  implementation("${edcGroup}:policy-monitor-store-sql:${edcVersion}")
-  implementation("${edcGroup}:sql-lease:${edcVersion}")
-  implementation("${edcGroup}:sql-pool-apache-commons:${edcVersion}")
-  implementation("${edcGroup}:transaction-local:$edcVersion")
-  implementation("${edcGroup}:transaction-datasource-spi:$edcVersion")
-  implementation ("org.postgresql:postgresql:42.7.2")
+  //implementation("${edcGroup}:asset-index-sql:${edcVersion}")
+  //implementation("${edcGroup}:contract-definition-store-sql:${edcVersion}")
+  //implementation("${edcGroup}:contract-negotiation-store-sql:${edcVersion}")
+  //implementation("${edcGroup}:contract-core:${edcVersion}")
+  //implementation("${edcGroup}:policy-definition-store-sql:${edcVersion}")
+  //implementation("${edcGroup}:policy-monitor-store-sql:${edcVersion}")
+  //implementation("${edcGroup}:sql-lease:${edcVersion}")
+  //implementation("${edcGroup}:sql-pool-apache-commons:${edcVersion}")
+  //implementation("${edcGroup}:transaction-local:$edcVersion")
+  //implementation("${edcGroup}:transaction-datasource-spi:$edcVersion")
+  //implementation ("org.postgresql:postgresql:42.7.2")
 }
 
 repositories {
