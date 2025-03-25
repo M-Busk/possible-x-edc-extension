af0b8b2a70f12992008461480972d8a10b49cfa1
M-Busk <125284318+M-Busk@users.noreply.github.com>
2025-03-25 15:10:30 +0100

    cleanup

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -42,23 +42,20 @@ dependencies {
     implementation(libs.edc.transfer.data.plane.signaling)
     implementation(libs.edc.transfer.pull.http.receiver)
     implementation(libs.edc.validator.data.address.http.data)
-
     implementation(libs.edc.data.plane.selector.api)
     implementation(libs.edc.data.plane.selector.core)
-
     implementation(libs.edc.data.plane.self.registration)
     implementation(libs.edc.data.plane.signaling.api)
     implementation(libs.edc.data.plane.public.api)
     implementation(libs.edc.data.plane.core)
     implementation(libs.edc.data.plane.http)
-
-
-
     implementation(libs.edc.data.plane.aws.s3)
     implementation(libs.edc.data.plane.signaling.client)
 
 
 
+	implementation(libs.edc.validator.data.address.aws.s3)
+	implementation(libs.edc.monitor.jdk.logger)
 	implementation(libs.edc.vault.hashicorp)
 }
 
